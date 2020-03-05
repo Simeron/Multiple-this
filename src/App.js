aaab60fdd29cd18cbfa90ac8cd62d7f796051f0e
Simeron Taak <simeron.taak@cancer.org.uk>
2020-03-05 17:46:16 +0000

    got some help from stack overflow and now the user selection displays in the console, and now I need to decide what I want to do with it next

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {MapThis} from './NumberGrid';
+import {MapThis, MultipleList} from './NumberGrid';
 
 function App() {
   return (
     <div className="App">
       <MapThis />
+      {/* <MultipleList /> */}
     </div>
   );
 }
--- src/NumberGrid.js
+++ src/NumberGrid.js
@@ -2,47 +2,105 @@ import React, { useEffect, useState, Fragment } from 'react';
 import './NumberGridStyles.css';
 
 // Get all the numbers from 1 - 144
+// let numbersStart = 1;
+// let numbersEnd = 144;
+// let allNumbers = Array(numbersEnd - numbersStart + 1)
+//     .fill()
+//     .map(() => numbersStart++);
+
+// export const MultipleList = () => {
+//     const numbersEnd = 144;
+//     const allNumbers = [...Array(numbersEnd).keys()].map(x => ++x);
+
+//     function multipleFinder(multipleno) {
+//         var multiples = [allNumbers];
+//         for (var i = 0; i < allNumbers.length; i++) {
+//           if (allNumbers[i] % multipleno === 0) {
+//             multiples.push(allNumbers[i]);
+//           }
+//         }
+//         return multiples;
+//     }
+
+//     const [multipleList, setMultipleList] = useState([]);
+
+//     useEffect(multipleList => {
+//         console.log(`render! ${multipleList}`);
+//       });
+    
+//       function handleOnClick(event, number) {
+//         event.preventDefault();
+//         setMultipleList(multipleFinder(number));
+//       }
+
+//       function handleStyle(number) {
+//         return multipleList.includes(number) ? "multiple" : "box";
+//       }
+
+//       function renderMultipleList() {
+//         return allNumbers.map(number => (
+//           <div
+//             className={handleStyle(number)}
+//             key={number.toString()}
+//             onClick={e => handleOnClick(e, number)}
+//           >
+//             {number}
+//           </div>
+//         ));
+//       }
+    
+//       return (
+//         <div>
+//           <div className="wrapper">{renderMultipleList()}</div>
+//           <div />
+//         </div>
+//       );
+//     };
+    
+//     export default MultipleList;
+
+    // Get all the numbers from 1 - 144
 let numbersStart = 1;
 let numbersEnd = 144;
 let allNumbers = Array(numbersEnd - numbersStart + 1)
     .fill()
     .map(() => numbersStart++);
 
-function MultipleFinder(allthenumbers, findmultipleof) {
+function MultipleFinder(allthenumbers, multipleno) {
     var multiples = [allNumbers];
     for (var i = 0; i < allthenumbers.length; i++) {
-        if (allthenumbers[i] % findmultipleof === 0) { // divide by the number
-            multiples.push(allthenumbers[i]); // add the current multiple found to the multiples array
+        if (allthenumbers[i] % multipleno === 0) {
+            multiples.push(allthenumbers[i]);
         }
     }
     return multiples;
 }
 // return MultipleFinder();
-console.log(MultipleFinder(allNumbers, 20)); // Output: [4, 6, 8]
+console.log(MultipleFinder(allNumbers, 20));
 
+    // Map all 1 - 144 numbers to the grid
+    export const MapThis = () => {
 
-// Map all 1 - 144 numbers to the grid
-export const MapThis = () => {
+        function showTheMultiples(number) {
+            // console.log(allNumbers);
+            MultipleFinder(allNumbers, number);
+            console.log(MultipleFinder(allNumbers, number))
 
-    function showTheMultiples(allNumbers) {
-        console.log(allNumbers);
-        MultipleFinder(allNumbers, 6);
-        console.log(MultipleFinder(allNumbers, 10))
-    }
-    return (
-        <div>
-            <div className="wrapper">
-                {allNumbers.map(allNumbers => (
-                    <div className="box" key={allNumbers.toString()} onClick={() => showTheMultiples(allNumbers)}>
-                        {allNumbers}
-                    </div>
-                ))}
-            </div>
+        }
+        return (
             <div>
+                <div className="wrapper">
+                    {allNumbers.map(number => (
+                        <div className="box" key={allNumbers.toString()} onClick={() => showTheMultiples(number)}>
+                            {number}
+                        </div>
+                    ))}
+                </div>
+                <div>
+                </div>
             </div>
-        </div>
-    )
-}
+        )
+    }
 
 // https://stackoverflow.com/questions/12491101/javascript-create-array-from-for-loop - looping nos
 // https://codepen.io/jh3y/pen/NeLrxr - changing background colour
