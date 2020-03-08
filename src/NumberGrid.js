4707aad9877ada43dacf6dc26d4e6d57806e3c1e
Simeron Taak <simeron.taak@cancer.org.uk>
2020-03-08 22:26:48 +0000

    Have clened up the files still a little left to do and I made the grid responsive

--- src/NumberGrid.js
+++ src/NumberGrid.js
@@ -8,6 +8,7 @@ let allNumbers = Array(numbersEnd - numbersStart + 1)
     .fill()
     .map(() => numbersStart++);
 
+// Find the multiples / pass through the right numbers to evalueate 
 export const MultipleList = () => {
     const numbersEnd = 144;
     const allNumbers = [...Array(numbersEnd).keys()].map(x => ++x);
@@ -15,49 +16,50 @@ export const MultipleList = () => {
     function multipleFinder(multipleno) {
         var multiples = [allNumbers];
         for (var i = 0; i < allNumbers.length; i++) {
-          if (allNumbers[i] % multipleno === 0) {
-            multiples.push(allNumbers[i]);
-          }
+            if (allNumbers[i] % multipleno === 0) {
+                multiples.push(allNumbers[i]);
+            }
         }
         return multiples;
     }
 
+
+    // Fuctionility to handle the selected number and to show to the user the multiples of the number selected
     const [multipleList, setMultipleList] = useState([]);
 
     useEffect(multipleList => {
         console.log(`render! ${multipleList}`);
-      });
-    
-      function handleOnClick(event, number) {
+    });
+
+    function handleOnClick(event, number) {
         event.preventDefault();
         setMultipleList(multipleFinder(number));
-      }
+    }
 
-      function handleStyle(number) {
+    function handleStyle(number) {
         return multipleList.includes(number) ? "multiple" : "box";
-      }
+    }
 
-      function renderMultipleList() {
+    function renderMultipleList() {
         return allNumbers.map(number => (
-          <div
-            className={handleStyle(number)}
-            key={number.toString()}
-            onClick={e => handleOnClick(e, number)}
-          >
-            {number}
-          </div>
+            <div
+                className={handleStyle(number)}
+                key={number.toString()}
+                onClick={e => handleOnClick(e, number)}
+            >
+                {number}
+            </div>
         ));
-      }
-    
-      return (
-        <div>
-          <div className="wrapper">{renderMultipleList()}</div>
-          <div />
+    }
+
+    return (
+        <div className="container">
+            <div className="wrapper">{renderMultipleList()}</div>
         </div>
-      );
-    };
-    
-    export default MultipleList;
+    );
+};
+
+export default MultipleList;
 
     // Get all the numbers from 1 - 144
 // let numbersStart = 1;
